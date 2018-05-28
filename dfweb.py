1407077c03f98fc9a6f3e8981767481b030b95d9
Amir <amirhossein.gh@gmail.com>
2018-05-28 20:17:02 +0200

    First version of cat/num columns

--- dfweb.py
+++ dfweb.py
@@ -14,8 +14,20 @@ def hello_world():
 
 @app.route('/')
 def analysis():
-    x = pd.DataFrame(np.random.randn(20, 5))
-    return render_template("analysis.html", name='amir', data=x)
+    # x = pd.DataFrame(np.random.randn(20, 5))
+    x = pd.read_csv('/Users/amir/projects/dfweb/data/iris.csv')
+    col_number =  x.columns.shape[0]
+    cat = assign_category(col_number)
+    data=(x.iloc[:6, :]).T
+    data.reset_index()
+    data.insert(0, 'category', cat)
+    return render_template("analysis.html", name='amir', data=data)
+
+
+def assign_category(col_numbers):
+    cat = np.random.choice(['numerical', 'categorical'], col_numbers)
+    return cat
+
 
 @app.route('/slider')
 def slider():
--- static/table.js
+++ static/table.js
@@ -1,18 +1,53 @@
+// $(document).ready(function () {
+//     var table = $('#amir').DataTable({
+//         'select': 'api'
+//     });
+//
+//     $('#amir').on('click', 'tbody td', function(){
+//        // If this column is selected
+//        if(table.column(this, { selected: true }).length){
+//           table.column(this).deselect();
+//
+//        // Otherwise, if this column is not selected
+//        } else {
+//           table.column(this).select();
+//        }
+//     });
+//     // table.on('select', function (e, dt, type, indexes) {
+//     //     var data = table.columns(col).data().pluck('id');
+//     //     console.log(data)
+//     //     // }
+//     // });
+// })
+// ;
+
+
+
 $(document).ready(function () {
-    var table = $('#amir').DataTable({
-        'select': 'api'
+
+    category = {
+        'categorical': '<select> <option value = "categorical" selected> Categorical </option> <option value = "numerical"> Numerical </option>',
+        'numerical': '<select> <option value = "categorical" > Categorical </option> <option value = "numerical" selected> Numerical </option>'
+    };
+
+
+    $('#amir').DataTable({
+        "columnDefs": [
+            {
+                "render": function (data, type, row) {
+                    return category[data];
+                },
+                "targets": 1
+            }
+        ],
+        'ordering': false
     });
 
-    $('#amir').on('click', 'tbody td', function(){
-       // If this column is selected
-       if(table.column(this, { selected: true }).length){
-          table.column(this).deselect();
 
-       // Otherwise, if this column is not selected
-       } else {
-          table.column(this).select();
-       }
-    });
+    // var table = $('#amir').DataTable({
+    //     'select': 'row'
+    // });
+
     // table.on('select', function (e, dt, type, indexes) {
     //     var data = table.columns(col).data().pluck('id');
     //     console.log(data)
@@ -20,4 +55,3 @@ $(document).ready(function () {
     // });
 })
 ;
-
